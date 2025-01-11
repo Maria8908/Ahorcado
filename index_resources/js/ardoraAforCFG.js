e572343ad26c8d2ad6db9f4c55ef57cfcbede945
Maria8908 <100075872@p.uapa.edu.do>
2025-01-10 17:28:57 -0800

    Add files via upload

---
+++ index_resources/js/ardoraAforCFG.js
@@ -0,0 +1,32 @@
+//Creado con Ardora - www.webardora.net
+//bajo licencia Attribution-NonCommercial-NoDerivatives 4.0 International (CC BY-NC-ND 4.0)
+//para otros usos contacte con el autor
+var timeAct=360; timeIni=360; timeBon=0;
+var successes=0; successesMax=6; attempts=0; attemptsMax=1;
+var score=0; scoreMax=6; scoreInc=1; scoreDec=0;
+var typeGame=0;
+var tiTime=false;
+var tiTimeType=1;
+var tiButtonTime=true;
+var textButtonTime="Comenzar";
+var tiSuccesses=false;
+var tiAttempts=false;
+var tiScore=false;
+var startTime;
+var colorBack="#C0C0C0"; colorButton="#91962F"; colorText="#400080"; colorSele="#FF8000";
+var goURLNext=false; goURLRepeat=false;tiAval=false;
+var scoOk=0; scoWrong=0; scoOkDo=0; scoWrongDo=0; scoMessage=""; scoPtos=10;
+var fMenssage="Verdana, Geneva, sans-serif";
+var fActi="Verdana, Geneva, sans-serif";
+var fEnun="Verdana, Geneva, sans-serif";
+var timeOnMessage=5; messageOk="Felicidades!"; messageTime=""; messageError=""; messageErrorG=""; messageAttempts=""; isShowMessage=false;
+var urlOk=""; urlTime=""; urlError=""; urlAttempts="";
+var goURLOk="_blank"; goURLTime="_blank"; goURLAttempts="_blank"; goURLError="_blank"; 
+borderOk="#008000"; borderTime="#FF0000";borderError="#FF0000"; borderAttempts="#FF0000";
+var wordsGame="aW5kZXg="; wordsStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+function giveZindex(typeElement){var valueZindex=0; capas=document.getElementsByTagName(typeElement);
+for (i=0;i<capas.length;i++){if (parseInt($(capas[i]).css("z-index"),10)>valueZindex){valueZindex=parseInt($(capas[i]).css("z-index"),10);}}return valueZindex;}
+var words=["RElHSVRBTA==","RURVQ0FDScOTTg==","T05TVFJVQ1RJVklTTU8g","Q09ORUNUSVZJU01P","Q09MQUJPUkFUSVZP","QUNUSVZP","","","",""];imaW=["","","","","","","","","",""];queW=["Relacionado con tecnologías electrónicas","Proceso de adquirir conocimientos y valore","Teoría donde el aprendizaje se construye activamente","Aprendizaje basado en redes digitales","Trabajo en equipo para alcanzar metas comunes","Participación dinámica en el aprendizaje","","","",""];var wordsL=[7,9,14,12,12,6,"","","",""];var altW=["","","","","","","","","",""];
+var auW=["","","","","","","","","",""];
+var fillLetter="ABCDEFGHIJKLMNÑOPQRSTUVWXYZÁÉÍÓÚÜ "; wordsG=[];var wordsGL=[]; indexG=0; actMaxWidth=550; indexWord=0; totalWord=0;
+var alterW=[];var imageW=[];questionW=[];audioW=[];profG=0; dirMedia="index_resources/media/"; textBNext="";
